52ec7cc9d963fb34fe5445c116370a999a137d53
Isaac Martínez <isaac.martinez6437@alumnos.udg.mx>
2025-01-05 02:38:08 -0600

    feat -> search github users implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,56 @@
-import { useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import './App.css'
+import User from './assets/components/user'
 
 function App() {
+  const [userName, setUserName] = useState("Martinezisaac")
+  const [userData, setUserData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  //Funcion para obtener la informacion del usuario de GitHub
+  async function fetchGitHubUsers() { 
+    setLoading(true);
+    const result = await fetch(`https://api.github.com/users/${userName}`); //Buscar los datos del username indicado
+    const data = await result.json(); //Obtener los datos
+    
+
+    if (data) { //Validar si existen datos
+      setUserData(data); //Obtener la informacion del usuario
+      setLoading(false); //Reiniciar el valor
+      setUserName(''); //Reiniciar los valores
+    }
+    console.log(data); //Imprimir los datos en consola
+  }
+
+  //Funcion para el manejo de las entradas
+  function handleSubmit() {
+    fetchGitHubUsers(); //Cargar los datos con 
+  } 
+
+  //Realizar la busqueda
+  useEffect(() => {
+    fetchGitHubUsers();
+  }, [])
+
+  if (loading) {
+    return <h1>Loading information</h1>
+  }
+
   return (
-    <div className='App'>
-      <div>Hola mundo, soy Isaac</div>
+    <div className='github-profile-container'>
+       <div className = "input-wrapper"> 
+        <input 
+          name = "input-wrapper"
+          type = "text"
+          placeholder = "Insert a GitHub user"
+          value = { userName }
+          onChange = { (event) => setUserName(event.target.value)}
+          />
+        <button onClick={ handleSubmit }>Search</button>
+      </div>
+      { userData !== null ? <User user = { userData } /> : null }
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/assets/components/user.jsx
@@ -0,0 +1,46 @@
+//Componente para mostrar la informacion del usuario de Github
+const User = ( { user } ) => {
+    const {
+        avatar_url,
+        followers,
+        following,
+        public_repos,
+        url,
+        name,
+        created_at,
+        login,
+    } = user; 
+
+    const createdDate = new Date(created_at);
+
+    return (
+        <div className="githubDetails">
+            <div className="avatarImage">
+                <img src = { avatar_url } className = "avatar" alt = "user avatar" />
+            </div>
+            <div className="nameLink">
+                <a href = { `https://github.com/${ login }` }> {name || login} </a>
+            </div>
+            <div className="userName">
+                <p> Nombre de usuario: { login } </p>
+            </div>
+            <div className="joinedDate">
+                <p> User Joined on {" "} { `${ createdDate.getDate() } ${ createdDate.toLocaleString("en-us", { month: "short", }) } ${ createdDate.getFullYear() }` }</p>
+            </div>
+            <div>
+                <p>Public Repos</p>
+                <p> {public_repos} </p>
+            </div>
+            <div>
+                <p>Followers</p>
+                <p> {followers} </p>
+            </div>
+            <div>
+                <p>Following</p>
+                <p> {following} </p>
+            </div>
+        </div>
+    );
+}
+
+export default User;
