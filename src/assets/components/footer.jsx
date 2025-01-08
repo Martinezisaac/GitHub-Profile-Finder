464495056d8edc6425cb152e6ed8000d1a403120
Isaac Martínez <isaac.martinez6437@alumnos.udg.mx>
2025-01-07 22:51:15 -0600

    style -> header and footer responive desdign for laptop

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
     return (
         <div className="footer-content">
-            <div className='content-wrapper'>
+            <div className='footer-wrapper'>
                 <div className='content-one'>
                     <img src= { github_logo } className='github-logo' />
                     <p> © { currentYear } GitHub Profile Finder. Inc </p> 
