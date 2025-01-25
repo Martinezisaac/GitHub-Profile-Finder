718cf2ac9b5e1294d28d69b3f6e2855eaf8fbcd2
Isaac Martínez <isaac.martinez6437@alumnos.udg.mx>
2025-01-25 01:30:04 -0600

    chore -> adding data for documentation

--- src/assets/components/status_components/welcome.jsx
+++ src/assets/components/status_components/welcome.jsx
@@ -4,15 +4,7 @@ const Welcome = () => {
     return (
         <div className="welcome-wrapper">
             <h1> Welcome to the GitHub profile Finder</h1>
-            <p>
-                These page is a personal project, you can search GitHub users and get their information, you can also watch their public repositories an visit them
-            </p>
-
-            <p>
-                - responsividad para movil y laptop y tablets
-                - infinite scroller
-                - barra de busqueda (mejorar el diseño para evitar que se cuatrapie)
-            </p> 
+            <p> Ya mero queda :) </p>
         </div>
     );
 }
